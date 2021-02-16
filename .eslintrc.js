2e518d86b04190794f9797a3e767303d3f0ee0db
yash_sewp <yashveersewparthab@gmail.com>
2021-02-16 11:01:32 +0200

    added  eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//   root: true,
-//   env: {
-//     node: true
-//   },
-//   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-//   parserOptions: {
-//     parser: "babel-eslint"
-//   },
-//   rules: {
-//     // "no-multiple-spaces": ["error"],
-//     "vue/no-multiple-template-root": "off",
-//     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-//     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-//   }
-// };
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    // "no-multiple-spaces": ["error"],
+    // "vue/no-multiple-template-root": "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
