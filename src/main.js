f02a6d62b464d7a996caf1c7c69ea523341e80aa
yash_sewp <yashveersewparthab@gmail.com>
2021-02-16 10:48:17 +0200

    Removed lodash references

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import VueLodash from 'vue-lodash';
-import lodash from 'lodash';
 import JwPagination from 'jw-vue-pagination';
 
 import './assets/styles/index.css';
 
-Vue.use(VueLodash, { lodash: lodash })
 Vue.component('jw-pagination', JwPagination);
 
 Vue.config.productionTip = false;
